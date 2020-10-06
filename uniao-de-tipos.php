ff6fb0d7f1af65f55dd5ee3f08f100339a3238e3
Elton Fonseca <elton869@gmail.com>
2020-10-06 09:57:39 -0300

    07 - Definindo tipo false na união de tipos

--- uniao-de-tipos.php
+++ uniao-de-tipos.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 class Marca{}
 
 class Carro {
+    protected int $velocidadeAtual = 0;
+
     public function __construct(
         protected string|Marca $marca, 
         protected string $cor,
@@ -16,11 +18,23 @@ class Carro {
     ): string|float {
         return $aceleracao . ' ' . $velocidadeMaxima;
     }
+
+    public function estaAndando(): int|false
+    {
+        if ($this->velocidadeAtual > 0) {
+            return $this->velocidadeAtual;
+        }
+
+        return false;
+    }
 }
 
 $uno = new Carro('Fiat', 'Preto',);
 
 echo $uno->andar(velocidadeMaxima: '110', aceleracao: '5',);
+
+echo "<br>";
+var_dump($uno->estaAndando());
 
 echo "<br>";
 var_dump($uno);
