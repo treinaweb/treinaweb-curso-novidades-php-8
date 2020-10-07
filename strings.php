4cc024e7338a4f92a80b41715b4b3006120d6767
Elton Fonseca <elton869@gmail.com>
2020-10-07 12:25:48 -0300

    18 - Novas funções para trabalhar com Strings

--- strings.php
+++ strings.php
@@ -4,3 +4,9 @@ $a = 1;
 $b = 2;
 
 echo "o valor de a+b é: " . $a + $b;
+echo "<br>";
+
+var_dump(str_contains('Treinaweb', 'ina'));
+var_dump(str_starts_with('Treinaweb', 'Treina'));
+var_dump(str_ends_with('Treinaweb', 'web'));
+
