aa875b5c7ee8664a51b96f1328181e0e37ea303a
Elton Fonseca <elton869@gmail.com>
2020-10-08 14:51:46 -0300

    21 - Realizando a validação de forma prática com atributo

--- atributos.php
+++ atributos.php
@@ -11,16 +11,46 @@ class Validacao {
         public string $regra,
         public int $valor
     ){}
-}
 
-$reflectionClass = new reflectionClass(Carro::class);
+    public static function validar(object $objetoParaValidar)
+    {
+        $reflectionClass = new reflectionClass($objetoParaValidar::class);
 
-foreach ($reflectionClass->getProperties() as $propriedade) {
-    $atributos = $propriedade->getAttributes();
+        foreach ($reflectionClass->getProperties() as $propriedade) {
+            $nomePropriedade = $propriedade->getName();
+            $atributos = $propriedade->getAttributes(self::class);
 
-    foreach ($atributos as $atributo) {
-        $instanciaValidacao = $atributo->newInstance();
+            foreach ($atributos as $atributo) {
+                $instanciaValidacao = $atributo->newInstance();
+                $nomeRegra = $instanciaValidacao->regra;
 
-        var_dump($instanciaValidacao);
+                $mensagemValidacao = Regras::$nomeRegra(
+                    $objetoParaValidar->$nomePropriedade,
+                    $instanciaValidacao->valor
+                );
+
+                echo "$nomePropriedade: $mensagemValidacao";
+            }
+        }
     }
 }
+
+class Regras 
+{
+    public static function max(int $valor, int $maximo): string
+    {
+        if ($valor > $maximo) {
+            return 'valor maior que o máximo';
+        }
+
+        return 'valor menor que o máximo';
+    }
+}
+
+$uno = new Carro;
+$uno->velocidade = 20;
+
+Validacao::validar($uno);
+
+
+
