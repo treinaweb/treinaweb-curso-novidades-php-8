7ba286d1d40f6db6d0ca84ad196c60038bdd7a8d
Elton Fonseca <elton869@gmail.com>
2020-10-08 14:27:21 -0300

    20 - Obtendo os dados declarados nos atributos

--- atributos.php
+++ atributos.php
@@ -4,3 +4,23 @@ class Carro {
     #[Validacao('max', 110)]
     public int $velocidade;
 }
+
+#[Attribute]
+class Validacao {
+    public function __construct(
+        public string $regra,
+        public int $valor
+    ){}
+}
+
+$reflectionClass = new reflectionClass(Carro::class);
+
+foreach ($reflectionClass->getProperties() as $propriedade) {
+    $atributos = $propriedade->getAttributes();
+
+    foreach ($atributos as $atributo) {
+        $instanciaValidacao = $atributo->newInstance();
+
+        var_dump($instanciaValidacao);
+    }
+}
